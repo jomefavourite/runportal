4ce4ff7363200089d63742c9491fabff50fc64b8
jomefavourite <54733225+jomefavourite@users.noreply.github.com>
2020-03-23 09:51:38 +0100

    code

---
+++ main.js
@@ -0,0 +1,13 @@
+var today = new Date();
+var year = today.getFullYear();
+
+var el = document.getElementById('footer')
+var msg= '<p> Copyright &copy' + year + ' <a href="#">Redeemer University.</a> All Rights Reserved.</p>';
+el.innerHTML = msg;
+
+var user = {
+    matricNo: "RUN/AAA/12/1234",
+    password: 12345
+}
+
+
