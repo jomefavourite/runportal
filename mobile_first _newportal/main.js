ff57ba87786b23d5fce596653ce6c38a0dd6bde4
jomefavourite <54733225+jomefavourite@users.noreply.github.com>
2020-04-21 18:01:37 +0100

    mobile_first

---
+++ mobile_first _newportal/main.js
@@ -0,0 +1,23 @@
+var today = new Date();
+var year = today.getFullYear();
+
+var el = document.getElementById('footer');
+var msg = '<p> Copyright &copy' + year + '<a href="#">Redeemer University.</a> All Rights Reserved.</p>';
+el.innerHTML = msg;
+
+var user = {
+  mactricNo: "RUN/AAA/12/1234",
+  password: 12345
+}
+
+function check() {
+  var inputMatric = document.getElementById('matric').value;
+  var inputPass = document.getElementById('pass').value;
+  var msg1 = "Signed In successful";
+  var msg2 = "Incorrect Details";
+  if(inputMatric === user.mactricNo && Number(inputPass) === user.password) {
+    var show = document.getElementById('show').textContent = msg1;
+  } else{
+    var show = document.getElementById('show').textContent = msg2;
+  }
+}
